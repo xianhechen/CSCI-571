67725dd0984649658f5f66849c070dfe4e7c65ba
Mike the Thug <chenxi28@msu.edu>
2017-02-24 12:38:12 -0800

    generated a page

---
+++ hw6/search.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+</head>
+
+<body>
+<div class="mainpage">
+<nav>
+	<ul class="left">
+		<li><a href="./">The Felis Agency</a></li>
+	</ul>
+
+	<ul class="right">
+		<li><a href="login.php">Log in</a></li>
+	</ul>
+</nav>
+
+<header class="main">
+
+</header>
+
+
+<form class="request">
+	<fieldset>
+		<legend>Request a free case evaluation!</legend>
+		<p>
+			<label for="name">Name</label><br>
+			<input type="text" id="name" name="name" placeholder="Name">
+		</p>
+		<p>
+			<label for="email">E-Mail</label><br>
+			<input type="email" id="email" name="email" placeholder="Email Address">
+		</p>
+		<p>
+			<label for="message">Message</label><br>
+			<textarea id="message" name="message" placeholder="Message"></textarea>
+		</p>
+		<p>
+			<input type="submit" value="Send">
+		</p>
+
+	</fieldset>
+</form>
+
+<footer>
+
+</footer>
+
+</div>
+
+</body>
+</html>
