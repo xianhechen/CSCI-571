7221b3f8c06ad438e0093eea8ac4207630fe9045
Mike the Thug <chenxi28@msu.edu>
2017-03-02 16:22:34 -0800

    doing toggle

--- hw6/search.php
+++ hw6/search.php
@@ -6,8 +6,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="format-detection" content="telephone=no">
-    <script>
+    <style>
+        html, body {
+            width: 100%;
+        }
 
+        fieldset {
+            margin: 0 auto;
+            width: 50%;
+            margin-bottom: 50px;
+        }
+
+        #usersTable, th, td {
+            border-collapse: collapse;
+            border: 1px solid black;
+            margin: 0 auto;
+            width: 60%;
+            text-align: left;
+        }
+
+        #albumsButton, #postsButton {
+            margin: 0 auto;
+            width: 60%;
+            text-align: center;
+        }
+
+        #albumsTable {
+            border-collapse: collapse;
+            border: 1px solid black;
+            margin: 0 auto;
+            width: 60%;
+            text-align: left;
+        }
+
+        #postsTable {
+            border-collapse: collapse;
+            border: 1px solid black;
+            margin: 0 auto;
+            width: 60%;
+            text-align: left;
+        }
+
+
+
+    </style>
+    <script>
         function selectPlaces() {
             var selection = document.getElementById("type");
             if (selection.options[selection.selectedIndex].value == "place") {
@@ -22,15 +65,39 @@
             window.location = "http://cs-server.usc.edu:16031/zuoyeliu/search.php";
         }
 
-        function hideElement(target1, target2) {
+        function hideElement(target2, target1) {
             //target1 is the element getting hidden
-            document.getElementById(target1).style.visibility = "hidden";
-            document.getElementById(target1).style.height = "0px";
-            document.getElementById(target2).style.visibility = "visible";
-            document.getElementById(target2).style.height = 'auto';
+                    //posts     //albums
+
+
+
+
+            var x = document.getElementById(target1);//albums
+            var y = document.getElementById(target2);//posts
+            if (x.style.visibility == "hidden" && y.style.visibility == "hidden") {
+                x.style.visibility = "visible";
+                x.style.height = "auto";
+            } else if (x.style.visibility == "visible" && y.style.visibility == "hidden") {
+                x.style.visibility = "hidden";
+                x.style.height = "0px";
+            } else if (x.style.visibility == "hidden" && y.style.visibility == "visible") {
+                y.style.visibility = "hidden";
+                y.style.height = "0px";
+            }
+
+
         }
 
-
+        function toggle(target) {
+            var x = document.getElementById(target);
+            if (x.style.visibility == "hidden") {
+                x.style.visibility = "visible";
+                x.style.height = auto;
+            } else {
+                x.style.visibility = "hidden";
+                x.style.height = "0px";
+            }
+        }
 
 
     </script>
@@ -77,7 +144,6 @@
                 } else if (isset($_GET['location'])) {
                     $loc = htmlentities($_GET['location']);
                 }
-
                 if (isset($_POST['distance'])) {
                     $dist = htmlentities($_POST['distance']);
                 } else if (isset($_GET['distance'])) {
@@ -105,7 +171,6 @@ HTML;
         $fields = '';
         $center = '';
         $distance = '';
-
         $access_token = 'EAACxu1kZBnhMBAFeLhHQbC1v46oD0qU0dtvLZAy0m0TGXhRXdggSzkf69U63KoQE8C7Sl86BpJJZCTsTvOylTiLyaLIJl3ZCARh5ZBf9Ad721r9qIZBdO3vJBcSENm0iVIb6xqj2NpLVWaKwTMPOtrSZAblVK5qqFIZD';
         $fb = new Facebook\Facebook([
             'app_id' => '195418214276627',
@@ -131,19 +196,29 @@ HTML;
         }
 
         function displayResult($result,$token) {
-
             if (sizeof($result) == 1 && sizeof($result['data']) == 0) {
                 echo "<p>No  Records  have  been found</p>";
-            } else {}
-            foreach ($result['data'] as $data) {
-                $pic_url = $data['picture']['data']['url'];
-                $locationP = '';
-                $distanceP = '';
-                if (isset($_POST['location']) && isset($_POST['distance'])) {
-                    $locationP = $_POST['location'];
-                    $distanceP = $_POST['distance'];
+            } else {
+                $html = <<<HTML
+                <table id='usersTable'>
+                <tr>
+                  <th>Profile Photo</th>
+                  <th>Name</th>
+                  <th>Detail</th>
+                </tr>
+HTML;
+                echo $html;
+                foreach ($result['data'] as $data) {
+                    $pic_url = $data['picture']['data']['url'];
+                    $locationP = '';
+                    $distanceP = '';
+                    if (isset($_POST['location']) && isset($_POST['distance'])) {
+                        $locationP = $_POST['location'];
+                        $distanceP = $_POST['distance'];
+                    }
+                    echo "<tr><td><a href=".$pic_url." target=\'_blank\'><img src='".$pic_url."' height='30px' width='30px'></a></td><td>".$data['name']."</td><td><a href='http://cs-server.usc.edu:16031/zuoyeliu/search.php?userid=".$data['id']."&keyword=".$_POST['keyword']."&type=".$_POST['type']."&location=".$locationP."&distance=".$distanceP."'> Details </a></td></tr>";
                 }
-                echo "<p>".$data['name']." ".$data['id']."</p><a href=".$pic_url." target=\'_blank\'><img src='".$pic_url."' height='30px' width='30px'></a><a href='http://cs-server.usc.edu:16031/zuoyeliu/search.php?userid=".$data['id']."&keyword=".$_POST['keyword']."&type=".$_POST['type']."&location=".$locationP."&distance=".$distanceP."'> Details </a></br>";
+                echo "</table>";
             }
         }
 
@@ -157,80 +232,80 @@ HTML;
 
 
 
+
+
         if (isset($_GET['userid']) && isset($_GET['keyword'])) {
             //echo $_GET['userid'];
             $userID =  $_GET['userid'];
             //echo $_GET['keyword'];
             $detailsURL = "https://graph.facebook.com/v2.8/".$userID."?fields=id,name,picture.width(700).height(700),albums.limit(5){name,photos.limit(2){name,picture}},posts.limit(5)&access_token=".$access_token;
             $urlContent = json_decode(file_get_contents($detailsURL), true);
-
             //var_dump($urlContent);
+            //echo "<div id='testArea'>";
 
-            echo "<div id='testArea'>";
+
+
             // commented for clean coding
             $albumsJSON = array();
             //var_dump($urlContent);
             if (isset($urlContent['albums'])){
+                //echo "<table id='albumsTable'>";
 
-                echo "<a href='#' onclick=\"hideElement('posts','albums')\">Album</a>";
-                echo "<div id='albums' style='visibility: hidden; height: 0px;'>";
+
+                echo "<div id='albumsButton'><a href='#' onclick='hideElement(\"postsTable\",\"albumsTable\")'>Albums</a></div>";
+                echo "<div id='albumsTable' style='visibility: hidden; height: 0px;'>";
+                echo "<table id='albumsTable'>";
                 //var_dump($urlContent);
                 foreach ($urlContent['albums'] as $albums) {
-
                     //var_dump($albums);
                     for ($i=0; $i!=5; $i++) {
                         if (isset($albums[$i])) {
                             array_push($albumsJSON, $albums[$i]);
                         } else {
-
                         }
                     }
                 }
-
-
                 foreach ($albumsJSON as $album) {
                     //var_dump(sizeof($album['photos']['data']));
                     if (isset($album['name'])) {
                         if (isset($album['photos']['data'])){
-                            echo "<a href='#'>".$album['name']."</a></br></br>";
+                            echo "<tr><td>";
+                            echo "<a href='#'>".$album['name']."</a></td></tr><tr><td>";
                             foreach ($album['photos']['data'] as $picture) {
                                 if (isset($picture['picture']) && isset($picture['id'])) {
                                     $picID = $picture['id'];
                                     $hiResRequest = "https://graph.facebook.com/v2.8/".$picID."/picture?access_token=".$access_token;
-                                    echo "<a href=".$hiResRequest." target='_blank'><img width='80px' height='80px' src=".$picture['picture']."></a> ";
+                                    echo "<a href=".$hiResRequest." target='_blank'><img width='80px' height='80px' src=".$picture['picture']."></a>";
                                 }
                             }
+                            echo "</td></tr>";
                         }
                         else {
                             echo $album['name']."</br></br>";
                         }
                     }
                     //var_dump($album);
-                    echo "</br>";
+                    //echo "</br>";
                 }
+                echo "</table>";
                 echo "</div>";
             } else {
+                echo "<table id='albumsTable'>";
                 echo "<div id='albums' style='visibility: hidden; height: 0px;'>";
                 echo "</div>";
-                echo "<p>No albums were found.</p>";
+                echo "<th><p>No albums were found.</p></th></table>";
                 //var_dump($urlContent);
             }
-
-
             echo "</div>";
-
-
-
-
-
 
             $postsJSON = array();
             if (isset($urlContent['posts'])){
-                echo "<a href='#' onclick=\"hideElement('albums','posts')\">Posts</a>";
-                echo "<div id='posts' style='visibility: hidden; height: 0px;'>";
+                //echo "<table id='postsTable'>";
 
+                echo "<div id='postsButton'><a href='#' onclick='hideElement(\"postsTable\",\"albumsTable\")'>Posts</a></div>";
+                echo "<div id='postsTable' style='visibility: hidden; height: 0px;'>";
+                echo "<table id='postsTable'>";
                 //echo "</div>";
-
                 //var_dump($urlContent['posts']);
                 foreach ($urlContent['posts'] as $posts) {
                     //var_dump($posts);
@@ -240,31 +315,25 @@ HTML;
                         }
                     }
                 }
-
-
                 foreach ($postsJSON as $post) {
                     if (isset($post['message'])) {
-                        echo $post['message']."</br></br>";
-                        echo "</br>";
+                        echo "<tr><td>".$post['message']."</td></tr>";
                     }
-
                 }
 
             } else {
+                echo "<table id='postsTable'>";
                 echo "<div id='posts' style='visibility: hidden; height: 0px;'>";
                 echo "</div>";
-                echo "<p>No posts were found.</p>";
+                echo "<th><p>No posts were found.</p></th></table>";
                 //var_dump($urlContent);
             }
-            echo "</p></div>";
+
             echo "</div>";
             //var_dump($albumsJSON);
         } else {
             //echo "not found";
         }
-
-
-
         if ($_SERVER["REQUEST_METHOD"] == "POST") { //form submitted
             $keyword = $_POST['keyword'];
             $type = $_POST['type'];
