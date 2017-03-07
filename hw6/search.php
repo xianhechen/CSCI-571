1e984a6cc1147aef7fff1fbda5498e5d87e11073
Mike the Thug <chenxi28@msu.edu>
2017-03-07 11:53:56 -0800

    i really think I'm done with HW6 this time.

--- hw6/search.php
+++ hw6/search.php
@@ -8,16 +8,14 @@
             width: 100%;
         }
         fieldset {
-            margin: 0 auto;
             width: 50%;
-            margin-bottom: 50px;
+            margin: 0 auto 50px;
         }
         #usersTable, th, td {
             border-collapse: collapse;
             border: 1px solid black;
             margin: 0 auto;
             width: 60%;
-            text-align: left;
         }
         #albumsButton, #postsButton {
             margin: 0 auto;
@@ -28,13 +26,11 @@
             border-collapse: collapse;
             margin: 0 auto;
             width: 60%;
-            text-align: left;
         }
         #postsTable {
             border-collapse: collapse;
             margin: 0 auto;
             width: 60%;
-            text-align: left;
         }
     </style>
     <script>
@@ -43,8 +39,7 @@
             if (selection.options[selection.selectedIndex].value == "place") {
                 document.getElementById("locationDetails").innerHTML = '<label for="location">Location</label><input type="text" id="location" name="location" placeholder="Location" required>';
                 document.getElementById("locationDetails").innerHTML += '<label for="distance"> Distance(meters)</label><input type="text" id="distance" name="distance" placeholder="Distance" required>'
-            }
-            else {
+            } else {
                 document.getElementById("locationDetails").innerHTML = '';
             }
         }
@@ -54,37 +49,34 @@
         function hideElement(target1, target2) {
             var x = document.getElementById(target1)
             var y = document.getElementById(target2)
-            if (x.style.visibility == "hidden")
-            {
+            if (x.style.visibility == "hidden") {
                 x.style.visibility = "visible";
                 x.style.height = "auto";
-                if (y.style.visibility == "visible")
-                {
+                if (y.style.visibility == "visible") {
                     y.style.visibility = "hidden";
                     y.style.height = "0px";
                 }
-            }
-            else {
+            } else {
                 x.style.visibility = "hidden";
                 x.style.height = "0px";
             }
+        }
+        function openInNewTab(url) {
+            var win = window.open( "", '_blank');
+            win.document.write("<img src='" + url + "'></img>");
+            win.focus();
         }
         function toggle(target) {
             var x = document.getElementById(target);
             if (x.style.display == "none") {
                 x.style.display = "inline";
-                x.style.height = "auto";
             } else {
                 x.style.display = "none";
-                x.style.height = "0px";
             }
         }
     </script>
 </head>
 <body>
-<div class="mainpage">
-<nav>
-</nav>
 <header class="main">
 </header>
 <form id = "request" class="request" method="post" action="search.php">
@@ -95,11 +87,9 @@
             <input type="text" id="keyword" name="keyword" placeholder="Keyword" required value="<?php
             if(isset($_POST['keyword'])) {
                 echo htmlentities($_POST['keyword']);
-            }
-            else if(isset($_GET['keyword'])) {
+            } else if(isset($_GET['keyword'])) {
                 echo htmlentities($_GET['keyword']);
-            }
-            ?>">
+            } ?>">
         </p>
         <p>
             <label for="type">Type</label>
@@ -113,7 +103,7 @@
         </p>
         <p id="locationDetails">
             <?php if((isset($_POST['type'])&& $_POST['type']=='place') || (isset($_GET['type'])&& $_GET['type']=='place')){
-                if (isset($_POST['location'])){
+                if (isset($_POST['location'])) {
                     $loc = htmlentities($_POST['location']);
                 } else if (isset($_GET['location'])) {
                     $loc = htmlentities($_GET['location']);
@@ -124,8 +114,8 @@
                     $dist = htmlentities($_GET['distance']);
                 }
                 $html = <<<HTML
-<label for="location">Location</label><input type="text" id="location" name="location" placeholder="Location" required value="$loc">
-<label for="distance"> Distance(meters)</label><input type="text" id="distance" name="distance" placeholder="Distance" required value="$dist">
+<label for="location">Location</label><input type="text" id="location" name="location" placeholder="Location" value="$loc">
+<label for="distance"> Distance(meters)</label><input type="text" id="distance" name="distance" placeholder="Distance" value="$dist">
 HTML;
                 echo $html;
             } ?>
@@ -134,7 +124,6 @@ HTML;
             <input type="submit" value="Search">
             <input type="button" value="Clear" onclick="resetForm()">
         </p>
-
     </fieldset>
     <div id="searchResult">
         <?php
@@ -144,29 +133,24 @@ HTML;
         $fields = '';
         $center = '';
         $distance = '';
-        $access_token = 'EAACxu1kZBnhMBAFeLhHQbC1v46oD0qU0dtvLZAy0m0TGXhRXdggSzkf69U63KoQE8C7Sl86BpJJZCTsTvOylTiLyaLIJl3ZCARh5ZBf9Ad721r9qIZBdO3vJBcSENm0iVIb6xqj2NpLVWaKwTMPOtrSZAblVK5qqFIZD';
+        $access_token = 'EAACxu1kZBnhMBADkNL93SOToHuupSjtmdFZBOWtrghZAWu6P2lGZCiaOfQnAQVOyQQnQfHpBnz9ei4tEOEs7652Rl4xR0D4MB0oimyC8sjivlEZAmoXVTPOSBBnh8XI6ZAdjXjSwr6UeixeIBs10uUxAMZCJ218nCUZD';
         $fb = new Facebook\Facebook([
-            'app_id' => '195418214276627',
-            'app_secret' => '990556fe84a777b6926f9c2df96bc7d6',
-            'default_graph_version' => 'v2.8',
-        ]);
+            'app_id' => '195418214276627', 'app_secret' => '990556fe84a777b6926f9c2df96bc7d6', 'default_graph_version' => 'v2.8']);
         $fb->setDefaultAccessToken($access_token);
-        function getResponse($request, $fb) {
+        function getResponse($request, $fb) { //connect and get the response from FB
             try {
                 $response = $fb->getClient()->sendRequest($request);
             } catch(Facebook\Exceptions\FacebookResponseException $e) {
-                // When Graph returns an error
                 echo 'Graph returned an error: ' . $e->getMessage();
                 exit;
             } catch(Facebook\Exceptions\FacebookSDKException $e) {
-                // When validation fails or other local issues
                 echo 'Facebook SDK returned an error: ' . $e->getMessage();
                 exit;
             }
             $graphNode = $response->getDecodedBody();
             return $graphNode;
         }
-        function displayTableHead() {
+        function displayTableHead() { //echo table head
             if ($_POST['type'] == 'event') {
                 $html = <<<HTML
                 <table id='usersTable'>
@@ -188,7 +172,7 @@ HTML;
             }
             echo $html;
         }
-        function displayResult($result) {
+        function displayResult($result) { //echo the full result form
             if (sizeof($result) == 1 && sizeof($result['data']) == 0) {
                 echo "<table id='usersTable'><tr><th>No Records have been found</th></tr></table>";
             } else {
@@ -198,9 +182,9 @@ HTML;
                         $pic_url = $data['picture']['data']['url'];
                         if (isset($data['place']['name'])) {
                             $eventLoc = $data['place']['name'];
-                            echo "<tr><td><a href=".$pic_url." target=\'_blank\'><img src='".$pic_url."' height='30px' width='30px'></a></td><td>".$data['name']."</td><td>".$eventLoc."</td></tr>";
+                            echo "<tr><td><div onclick='openInNewTab(\"".$pic_url."\")'><img src='".$pic_url."' height='30px' width='30px'></div></td><td>".$data['name']."</td><td>".$eventLoc."</td></tr>";
                         } else {
-                            echo "<tr><td><a href=".$pic_url." target=\'_blank\'><img src='".$pic_url."' height='30px' width='30px'></a></td><td>".$data['name']."</td><td></td></tr>";
+                            echo "<tr><td><div onclick='openInNewTab(\"".$pic_url."\")'><img src='".$pic_url."' height='30px' width='30px'></div></td><td>".$data['name']."</td><td></td></tr>";
                         }
                     }
                     echo "</table>";
@@ -214,20 +198,20 @@ HTML;
                             $locationP = $_POST['location'];
                             $distanceP = $_POST['distance'];
                         }
-                        echo "<tr><td><a href=".$pic_url." target=\'_blank\'><img src='".$pic_url."' height='30px' width='30px'></a></td><td>".$data['name']."</td><td><a href='search.php?userid=".$data['id']."&keyword=".$_POST['keyword']."&type=".$_POST['type']."&location=".$locationP."&distance=".$distanceP."'> Details </a></td></tr>";
+                        echo "<tr><td><div onclick='openInNewTab(\"".$pic_url."\")'><img src='".$pic_url."' height='30px' width='30px'></div></td><td>".$data['name']."</td><td><a href='search.php?userid=".$data['id']."&keyword=".$_POST['keyword']."&type=".$_POST['type']."&location=".$locationP."&distance=".$distanceP."'> Details </a></td></tr>";
                     }
                     echo "</table>";
                 }
             }
         }
-        function findCenter($string){
+        function findCenter($string){ //find center using Google Maps API
             $string = str_replace (" ", "+", urlencode($string));
             $details_url = "https://maps.googleapis.com/maps/api/geocode/json?address=".$string."&key=AIzaSyBSZNvwwsgMZf6a8l6-gtFAdLjus34W19I";
             $urlContent = json_decode(file_get_contents($details_url), true);
             $result = $urlContent['results'][0]['geometry']['location']['lat'].",".$urlContent['results'][0]['geometry']['location']['lng'];
             return $result;
         }
-        if (isset($_GET['userid']) && isset($_GET['keyword'])) {
+        if (isset($_GET['userid']) && isset($_GET['keyword'])) { //see if $_GET was set, if it was set then its not the original page
             $userID =  $_GET['userid'];
             $detailsURL = "https://graph.facebook.com/v2.8/".$userID."?fields=id,name,picture.width(700).height(700),albums.limit(5){name,photos.limit(2){name,picture}},posts.limit(5)&access_token=".$access_token;
             $urlContent = json_decode(file_get_contents($detailsURL), true);
@@ -240,43 +224,37 @@ HTML;
                     for ($i=0; $i!=5; $i++) {
                         if (isset($albums[$i])) {
                             array_push($albumsJSON, $albums[$i]);
-                        } else {
                         }
                     }
                 }
-                foreach ($albumsJSON as $album) {
+                foreach ($albumsJSON as $album) { //print table of albums
                     if (isset($album['name'])) {
                         if (isset($album['photos']['data'])){
                             echo "<tr><td>";
-                            //var_dump($album);
-                            echo "<a href='#' onclick=toggle('".urlencode($album['name'])."')>".$album['name']."</a></td></tr>";
-                            echo "<td id='".urlencode($album['name'])."' style='display: none;'>";
+                            echo "<a href='#' onclick=toggle('".urlencode($album['name'])."')>".$album['name']."</a></br>";
+                            echo "<div id='".urlencode($album['name'])."' style='display:none'>";
                             foreach ($album['photos']['data'] as $picture) {
                                 if (isset($picture['picture']) && isset($picture['id'])) {
                                     $picID = $picture['id'];
                                     $hiResRequest = "https://graph.facebook.com/v2.8/".$picID."/picture?access_token=".$access_token;
-                                    echo "<a href=".$hiResRequest." target='_blank'><img width='80px' height='80px' src=".$picture['picture']."></a>";
+                                    echo "<a onclick='openInNewTab(\"".$hiResRequest."\")'><img width='80px' height='80px' src=".$picture['picture']."></a>";
                                 }
                             }
-                            echo "</td>";
-                            echo "</td></tr>";
-                        }
-                        else {
-                            echo $album['name']."</br></br>";
+                            echo "</div></td></tr>";
+                        } else {
+                            echo "<tr><td>".$album['name']."</td></tr>";
                         }
                     }
                 }
-                echo "</table>";
-                echo "</div>";
+                echo "</table></div>";
             } else {
                 echo "<table id='albumsTable'>";
-                echo "<div id='albums' style='visibility: hidden; height: 0px;'>";
-                echo "</div>";
+                echo "<div id='albums' style='visibility: hidden; height: 0px;'></div>";
                 echo "<th><p>No albums were found.</p></th></table>";
             }
             echo "</div>";
             $postsJSON = array();
-            if (isset($urlContent['posts'])){
+            if (isset($urlContent['posts'])){ //print table of posts
                 echo "<div id='postsButton'><a href='#' onclick='hideElement(\"postsTable\",\"albumsTable\")'>Posts</a></div>";
                 echo "<div id='postsTable' style='visibility: hidden; height: 0px;'>";
                 echo "<table id='postsTable'>";
@@ -294,42 +272,34 @@ HTML;
                 }
             } else {
                 echo "<table id='postsTable'>";
-                echo "<div id='posts' style='visibility: hidden; height: 0px;'>";
-                echo "</div>";
+                echo "<div id='posts' style='visibility: hidden; height: 0px;'></div>";
                 echo "<th><p>No posts were found.</p></th></table>";
             }
-
             echo "</div>";
-        } else {
-            //echo "not found";
         }
-        if ($_SERVER["REQUEST_METHOD"] == "POST") { //form submitted
+        if ($_SERVER["REQUEST_METHOD"] == "POST") { //form submitted, handle requests
             $keyword = $_POST['keyword'];
             $type = $_POST['type'];
             if ($type == 'place') {
-                $center = $_POST['location'];
-                $distance = $_POST['distance'];
-                $latNlong = findCenter($center);
-                $request = $fb->request('GET', '/search', ['q' => $keyword, 'type' => $type, 'center' => $latNlong, 'distance' => $distance, 'fields'=>'id,name,picture.width(700).height(700)']);
+                if ($_POST['location'] != "" && $_POST['location'] != "") {
+                    $latNlong = findCenter($_POST['location']);
+                    $request = $fb->request('GET', '/search', ['q' => $keyword, 'type' => $type, 'center' => $latNlong, 'distance' => $_POST['distance'], 'fields'=>'id,name,picture.width(700).height(700)']);
+                } else {
+                    $request = $fb->request('GET', '/search', ['q' => $keyword, 'type' => $type, 'fields'=>'id,name,picture.width(700).height(700)']);
+                }
                 $result = getResponse($request, $fb);
                 displayResult($result);
-            }
-            else if ($type == 'event') {
+            } else if ($type == 'event') {
                 $request = $fb->request('GET', '/search', ['q' => $keyword, 'type' => $type, 'fields'=>'id,name,picture.width(700).height(700), place']);
                 $result = getResponse($request, $fb);
                 displayResult($result);
-            }
-            else {
+            } else {
                 $request = $fb->request('GET', '/search', ['q' => $keyword, 'type' => $type, 'fields'=>'id,name,picture.width(700).height(700)']);
                 $result = getResponse($request, $fb);
                 displayResult($result);
             }
-        }
-        ?>
+        } ?>
     </div>
 </form>
-<footer>
-</footer>
-</div>
 </body>
 </html>
