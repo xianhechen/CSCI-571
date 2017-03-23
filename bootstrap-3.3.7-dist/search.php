c0fb4813e76b3fca0eca44076ff952205bc0385a
xianhechen <chenxi28@msu.edu>
2017-03-22 21:23:18 -0700

    json was written

--- bootstrap-3.3.7-dist/search.php
+++ bootstrap-3.3.7-dist/search.php
@@ -3,12 +3,43 @@ $keyword = "";
 $type = "";
 $fields = "";
 $access_token = 'EAACxu1kZBnhMBADkNL93SOToHuupSjtmdFZBOWtrghZAWu6P2lGZCiaOfQnAQVOyQQnQfHpBnz9ei4tEOEs7652Rl4xR0D4MB0oimyC8sjivlEZAmoXVTPOSBBnh8XI6ZAdjXjSwr6UeixeIBs10uUxAMZCJ218nCUZD';
+
+$users = array();
+
 if (isset($_GET['keyword']) && isset($_GET['type'])) {
+
   $keyword = $_GET['keyword'];
   $type = $_GET['type'];
   $fields = "id,name,picture.width(700).height(700)";
   $url = "https://graph.facebook.com/v2.8/search?q=".$keyword."&type=".$type."&fields=".$fields."&access_token=".$access_token;
-  echo file_get_contents($url);
-
+  $result = json_decode(file_get_contents($url), true);
+  //echo gettype($result);
+  if (sizeof($result) == 1 && sizeof($result['data']) == 0) {
+      echo "<table id='usersTable'><tr><th>No Records have been found</th></tr></table>";
+  } else {
+      if ($_GET['type'] == 'event') {
+          echo displayTableHead();
+          foreach ($result['data'] as $data) {
+              $pic_url = $data['picture']['data']['url'];
+              if (isset($data['place']['name'])) {
+                  $eventLoc = $data['place']['name'];
+                  echo "<tr><td><div onclick='openInNewTab(\"".$pic_url."\")'><img src='".$pic_url."' height='30px' width='30px'></div></td><td>".$data['name']."</td><td>".$eventLoc."</td></tr>";
+              } else {
+                  echo "<tr><td><div onclick='openInNewTab(\"".$pic_url."\")'><img src='".$pic_url."' height='30px' width='30px'></div></td><td>".$data['name']."</td><td></td></tr>";
+              }
+          }
+      } else {
+          foreach ($result['data'] as $data) {
+              $user = array();
+              $user['id']=$data['id'];
+              $user['name']=$data['name'];
+              array_push($users, $user);
+          }
+          //print_r ($users);
+          header('Content-Type: application/json');
+          $done = json_encode($users,JSON_PRETTY_PRINT);
+          echo $done;
+      }
+  }
 }
 ?>
